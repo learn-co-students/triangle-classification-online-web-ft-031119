1d4a7de6cf49d4c2ce4162899bfa29e3f823beee
Eric Storey <estorey@gmail.com>
2019-03-28 23:30:59 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :sides
+  
+  def initialize(l1, l2, l3)
+    @sides=[l1, l2, l3]
+    @sides.sort!
+  end
+  
+  def kind 
+    if is_valid?
+      if @sides.uniq.length==1 
+        :equilateral
+      elsif @sides.uniq.length==2
+        :isosceles
+      else 
+        :scalene
+      end
+    end
+  end
+  
+  def is_valid?
+    if @sides[0]+@sides[1]<=@sides[2]|| sides.include?(0)
+      raise TriangleError
+    else
+      true 
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
+  
 end
